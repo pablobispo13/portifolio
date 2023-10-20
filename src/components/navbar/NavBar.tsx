7c4163eb6c992cdaed1528054120c3c4a501f2fb
Pablo Bispo <62626543+pablobispo13@users.noreply.github.com>
2023-10-20 11:51:29 -0300

    Removendo handleOpenUserMenu que nunca era usado

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -25,9 +25,6 @@ export const ResponsiveAppBar = () => {
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
